b6adf30cef27eea59a2c18ef7fe34fdb821721ec
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-04 20:09:01 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//these give us access to the objects in the html
+const mainarea = document.querySelector("#mainarea");
+const circle = document.querySelector("#circle");
+const square = document.querySelector("#square");
+const sometext1 = document.querySelector("#sometext1");
+var count=0;
+
+//the main function
 function runloop(){
   var newPosition=20+count*20;
   console.log("DEBUG: I am moving the square to "+newPosition+"px");
@@ -13,3 +21,5 @@ function runloop(){
     },3000)
   }
 }
+//run the loop every second, this will count to 10 and start again by setting count equal to zero.
+setInterval(runloop,1000)
