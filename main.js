04f1fade227982a4c1155326525372fdc7959576
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-04 19:48:42 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+//these give us access to the objects in the html
 const mainarea = document.querySelector("#mainarea");
 const circle = document.querySelector("#circle");
 const square = document.querySelector("#square");
 const sometext1 = document.querySelector("#sometext1");
 var count=0;
+
+//the main function
 function runloop(){
-  square.style.left=""+(count*10)+"px";
+  square.style.left=""+(20+count*20)+"px";
   count=count+1;
   if(count>10){
     count=0;
   }
 }
-
 //run the loop every second
 setInterval(runloop,1000)
