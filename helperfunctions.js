7e8dc35e7be5f07f7925356cf9c82742b0377cb3
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-05 12:53:43 +1000

    Update helperfunctions.js

--- helperfunctions.js
+++ helperfunctions.js
@@ -1,3 +1,20 @@
+/*
+/**
+ * Does something nifty.
+ * for example, the below will move circle from 20px to 300px over and over.
+ * bounceball(){
+ *     animate(function(progress){
+ *       circle.style.top = 20 + progress * 300 + 'px';
+ *     },1000).then(bounceball);
+ * }
+ * @param   {function} draw  A function that is called to animate your object, it contains a float, between 0-1, 
+ *                     used in your code to set how far your animation should move as a ration of the total move
+ *                     you want to make.
+ * @param   {number} duration  How long should the animation last.
+ * @param   {function} timing  an [optional] function that can manipulate the timing of the loop [default]=liniar.
+ * @returns {Promise} a promise you can use "then" to know when it is complete.
+ */
+
 function animate(draw, duration,timing) {
 
   return new Promise((resolve, reject) => {
