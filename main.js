29cf9fd103cf80cc79defadf3d3f5a52dbf02d5d
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-05 10:00:11 +1000

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const mainarea = document.querySelector("#mainarea");
 const circle = document.querySelector("#circle");
 const square = document.querySelector("#square");
 const sometext1 = document.querySelector("#sometext1");
+const startbutton = document.querySelector("#startbutton");
 var count=0;
 
 //this is a simple example of a function that will be called 4 times a second
 //you can do what you want in here, "setInterval" controls how often it is called.
 function runloop(){
-  var newPosition=20+count*10;
+  var newPosition=20+count*20;
   console.log("DEBUG: I am moving the square to "+newPosition+"px");
   square.style.left=""+newPosition+"px";
   count=count+1;
@@ -22,10 +23,9 @@ function runloop(){
     },3000);
   }
 }
-//run the loop every 250ms, this will count to 20 and start again by setting count equal to zero.
-setInterval(runloop,250);
 
-//this is a more complicated example. It calls the animate function, passing a function that moves the circle 
+
+//this is a more complicated example function. It calls the animate function, passing a function that moves the circle 
 //down for 1000ms 'then' when that is done moves it up for 1000ms, then calls the function again and again and again.
 function bounceball()
 {
@@ -37,6 +37,15 @@ function bounceball()
       },1000).then(bounceball);
   });
 }
-//this is the first call of bounce ball, that will then call itself, forever.
-bounceball();
+
+
+startbutton.onclick=function(){
+
+  //run 'runloop' every 250ms, this will count to 20 and start again by setting count equal to zero.
+  setInterval(runloop,250);
+
+  //this starts a recursive loop calling 'bounceball'.
+  bounceball();
+  
+}
 
