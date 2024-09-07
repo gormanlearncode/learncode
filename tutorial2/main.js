92d9c2b77a69a79005b37bb2e92a8b2a8fc9d269
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-07 15:03:44 +1000

    fff

--- tutorial2/main.js
+++ tutorial2/main.js
@@ -2,16 +2,16 @@ async function whenLoaded()
 {
     console.log("start");
     var sprite = Sprite.create({image:"images/squirrel.png",width:100,height:100});
-    var backdrop = Backdrop.create({backgroundCOlor:"yellow",width:800,height:400});
+    var backdrop = Backdrop.create({backgroundColor:"yellow",width:800,height:400});
     backdrop.addSprite(sprite);
     sprite.moveTo(100,100);
-    sprite.move(100);
+    sprite.move(100); 
     sprite.turnClockWise(10);
     sprite.move(100);
     sprite.turnClockWise(10);
     sprite.move(100);
     sprite.turnClockWise(10);
-    sprite.move(100);
+    sprite.move(100);   
     sprite.turnClockWise(10);
 
 }
