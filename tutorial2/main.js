122c02d5d38488997cab84aa6d277ebdf5e89260
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-07 15:32:11 +1000

    Update main.js

--- tutorial2/main.js
+++ tutorial2/main.js
@@ -1,23 +1,8 @@
-async function whenLoaded()
-{
-    console.log("start");
-    var sprite = Sprite.create({image:"images/squirrel.png",width:100,height:100});
-    var backdrop = Backdrop.create({backgroundColor:"yellow",width:800,height:400});
-    backdrop.addSprite(sprite);
-    sprite.moveTo(100,100);
-    for(let i=0;i<1000;i++)
-    {
-        sprite.move(10); 
-        //sprite.turnClockWise(2);
-        console.log("x");
-        await Controls.wait(200);
-    }
-
-
-
-}
-console.log("whenLoaded");
-
-window.onload = function() {
-    whenLoaded();
-}
+console.log("start");
+var sprite = Sprite.create({image:"images/squirrel.png",width:100,height:100});
+var backdrop = Backdrop.create({backgroundColor:"yellow",width:800,height:400});
+backdrop.addSprite(sprite);
+sprite.moveTo(100,100);
+await Controld.wait(1000);
+sprite.moveTo(200,100);
+console.log("end");
