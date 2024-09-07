066fe18a79c00bb76fc97d27f857358899199c70
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-07 15:56:01 +1000

    Update main.js

--- tutorial2/main.js
+++ tutorial2/main.js
@@ -1,19 +1,19 @@
 console.log("start");
 async function run(){
-  var sprite = Sprite.create({image:"images/squirrel.png",width:100,height:100});
+  var sprite1 = Sprite.create({image:"images/squirrel.png",width:100,height:100});
   var backdrop = Backdrop.create({backgroundColor:"yellow",width:800,height:400});
-  backdrop.addSprite(sprite);
-  sprite.moveTo(100,100);
+  backdrop.addSprite(sprite1);
+  sprite1.moveTo(100,100);
   console.log("moveTo 100");
   await Controls.wait(1000);
   console.log("moveTo 200");
-  sprite.moveTo(200,100);
+  sprite1.moveTo(200,100);
   await Controls.wait(1000);
   console.log("moveTo 300");
-  sprite.moveTo(300,100);
+  sprite1.moveTo(300,100);
   await Controls.wait(1000);
   console.log("moveTo 400");
-  sprite.moveTo(400,100);
+  sprite1.moveTo(400,100);
   console.log("end");
 }
 run();
