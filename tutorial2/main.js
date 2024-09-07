9f0c5f976e4c58ee82bf97e13407f5e6fcd44975
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-07 10:01:37 +1000

    ddd

--- tutorial2/main.js
+++ tutorial2/main.js
@@ -1,5 +1,6 @@
 function *whenLoaded()
 {
+    console.log("start");
     var sprite=Sprite.create({image:"images/squirrel.png",width:100,height:100});
     var backdrop=Backdrop.create({backgroundCOlor:"yellow",width:800,height:400});
     backdrop.addSprite(sprite);
