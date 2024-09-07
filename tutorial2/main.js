65f838d3dfe3040b41eb4a95f2c49e5f70c8cc15
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-07 13:42:21 +1000

    ddd

--- tutorial2/main.js
+++ tutorial2/main.js
@@ -5,11 +5,21 @@ function* whenLoaded()
     var backdrop = Backdrop.create({backgroundCOlor:"yellow",width:800,height:400});
     backdrop.addSprite(sprite);
     sprite.moveTo(100,100);
+    let count=0;
     while(true)
     {
         sprite.move(100);
-        yield;
+        yield count;
+        count++;
     }
 }
 console.log("whenLoaded");
 whenLoaded();
+
+const gen = whenLoaded();
+
+console.log(gen.next().value);
+// Expected output: 10
+
+console.log(gen.next().value);
+// Expected output: 20
