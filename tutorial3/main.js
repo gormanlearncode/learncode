26e91744eb6d0d342d52dba4d4a746eedda0f7d9
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-07 16:35:24 +1000

    Create main.js

---
+++ tutorial3/main.js
@@ -0,0 +1,19 @@
+console.log("start");
+async function run(){
+     var sprite = Sprite.create({image:"images/squirrel.png",width:100,height:100});
+     var backdrop = Backdrop.create({backgroundColor:"yellow",width:800,height:400});
+     backdrop.addSprite(sprite);
+     for(let i=0;i<10;i++)
+     {
+          let x=100+i*10;
+          sprite.moveTo(x,100);
+          await Controls.wait(100);
+     }
+     console.log("end");
+}
+let button1=document.createElement("button");
+document.setInnerText="RUN";
+document.body.append(button1);
+button1.onclick(()=>{
+    run();
+})
