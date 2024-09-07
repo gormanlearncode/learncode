91d18d5ebe770808cec77622e3030715af228ff4
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-07 16:47:47 +1000

    Update main.js

--- tutorial3/main.js
+++ tutorial3/main.js
@@ -1,8 +1,8 @@
 console.log("start");
+var sprite = Sprite.create({image:"images/squirrel.png",width:100,height:100});
+var backdrop = Backdrop.create({backgroundColor:"yellow",width:800,height:400});
+backdrop.addSprite(sprite);
 async function run(){
-     var sprite = Sprite.create({image:"images/squirrel.png",width:100,height:100});
-     var backdrop = Backdrop.create({backgroundColor:"yellow",width:800,height:400});
-     backdrop.addSprite(sprite);
      for(let i=0;i<10;i++)
      {
           let x=100+i*10;
