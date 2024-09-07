ed0d005379b38fca2e32bd717ca0c8cfad6aa9fa
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-07 15:46:27 +1000

    Update main.js

--- tutorial2/main.js
+++ tutorial2/main.js
@@ -4,8 +4,16 @@ async function run(){
   var backdrop = Backdrop.create({backgroundColor:"yellow",width:800,height:400});
   backdrop.addSprite(sprite);
   sprite.moveTo(100,100);
+  console.log("moveTo 100");
   await Controls.wait(1000);
+  console.log("moveTo 200");
   sprite.moveTo(200,100);
+  await Controls.wait(1000);
+  console.log("moveTo 300");
+  sprite.moveTo(300,100);
+  await Controls.wait(1000);
+  console.log("moveTo 400");
+  sprite.moveTo(400,100);
   console.log("end");
 }
 run();
