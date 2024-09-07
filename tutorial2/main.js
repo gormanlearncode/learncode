b56310b3124f41cd5aeec6399ea4e3b392e64b0d
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-07 15:32:27 +1000

    Update main.js

--- tutorial2/main.js
+++ tutorial2/main.js
@@ -3,6 +3,6 @@ var sprite = Sprite.create({image:"images/squirrel.png",width:100,height:100});
 var backdrop = Backdrop.create({backgroundColor:"yellow",width:800,height:400});
 backdrop.addSprite(sprite);
 sprite.moveTo(100,100);
-await Controld.wait(1000);
+await Controls.wait(1000);
 sprite.moveTo(200,100);
 console.log("end");
