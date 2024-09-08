950e50e011a56f6a39cb2f3f02a91b07aa6f441d
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-08 19:19:47 +1000

    Update main.js

--- tutorial3/main.js
+++ tutorial3/main.js
@@ -4,7 +4,7 @@ var backdrop = Backdrop.create({backgroundColor:"yellow",width:800,height:400});
 backdrop.addSprite(sprite);
 sprite.moveTo(10,100);
 async function run(){
-     for(let i=0;i<50;i++)
+     for(let i=0;i<60;i++)
      {
           let x=10+i*10;
           sprite.moveTo(x,100);
