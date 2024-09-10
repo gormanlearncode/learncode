f984fe9706dd96e05f798e3ca1a11c118078d2c3
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-10 17:14:12 +1000

    Update main.js

--- tutorial6/main.js
+++ tutorial6/main.js
@@ -9,6 +9,6 @@ backdrop.addSprite(puck);
       puck.move(10);
       puck.ifOnEdgeBounce();
       await Controls.wait(1);
-      //force hte loop to free up the CPU fir at least 1 tick.
+      //force the loop to free up the CPU fir at least 1 tick.
     }
 })();
