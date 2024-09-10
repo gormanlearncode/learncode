6a9fe40f070b64aae60b3fa91361f3bee9c7c1bb
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-10 17:36:31 +1000

    Update main.js

--- tutorial6/main.js
+++ tutorial6/main.js
@@ -2,14 +2,12 @@ let backdrop=new Backdrop("#backdrop");
 let puck=new Sprite("#puck");
 backdrop.addSprite(puck);
 
-(async ()=>{
-    puck.setDirection(100);
-    while(true)
-    {
-      if(Math.random()>0.8)puck.turnClockwise(1);
-      puck.move(20);
-      puck.ifOnEdgeBounce();
-      await Controls.wait(50);
-      //force the loop to free up the CPU for at least 1 tick.
-    }
-})();
+puck.setDirection(100);
+function step()
+{
+    if(Math.random()>0.8)puck.turnClockwise(1);
+    puck.move(20);
+    puck.ifOnEdgeBounce();
+    requestAnimationFrame(step);
+}
+requestAnimationFrame(step);
