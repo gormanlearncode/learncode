9309f35d5480c71f3359f43e645d6c9829abb4a5
gormanlearncode <craig.gorman.au@gmail.com>
2024-09-10 16:15:37 +1000

    Update main.js

--- tutorial6/main.js
+++ tutorial6/main.js
@@ -1,1 +1,13 @@
+let field=new Backdrop("#field");
+let puck1=new Sprite("#puck");
+field.addSprite(puck);
 
+(async ()=>{
+    puck.setDirection(100);
+    while(true)
+    {
+      puck.move(10);
+      puck.ifOnEdgeBounce();
+      await Controls.wait(20);
+    }
+})();
